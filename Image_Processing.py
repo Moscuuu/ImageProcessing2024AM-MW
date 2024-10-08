fe5e6314485d58c41fc2f46a13e37b7b444522fa
247034 Aleksander Moszyński <247034@edu.p.lodz.pl>
2024-10-08 15:47:54 +0200

    First implementations of three functionalities

---
+++ Image_Processing.py
@@ -0,0 +1,66 @@
+from PIL import Image
+import numpy as np
+import sys
+
+######################
+# FUNCTION DEFINITIONS
+######################
+
+def doBrightness(arr, param):
+    print("Function doBrightness invoked with param: " + param)
+    arr = (arr + float(param)).astype(np.int64)
+    arr[arr > 255] = 255
+    arr[arr < 0] = 0
+    return arr
+
+def doContrast(arr, param):
+    print("Function doContrast invoked with param: " + param)
+    arr = (arr * float(param)).astype(np.int64)
+    arr[arr > 255] = 255
+    arr[arr < 0] = 0
+    return arr
+
+def doNegation(arr):
+    print("Function doNegation invoked with param: ")
+    arr = 255 - arr
+    arr[arr > 255] = 255
+    arr[arr < 0] = 0
+    return arr
+
+###########################
+# HERE THE MAIN PART STARTS
+###########################
+#im = Image.open("lena.bmp")
+im = Image.open("lenac.bmp")
+
+arr = np.array(im.getdata())
+if arr.ndim == 1: #grayscale
+    numColorChannels = 1
+    arr = arr.reshape(im.size[1], im.size[0])
+else:
+    numColorChannels = arr.shape[1]
+    arr = arr.reshape(im.size[1], im.size[0], numColorChannels)
+
+if len(sys.argv) == 1:
+    print("No command line parameters given.\n")
+    sys.exit()
+
+if len(sys.argv) == 2:
+    if sys.argv[1] == '--negation':
+        arr = doNegation(arr)
+    else:
+        print("Too few command line parameters given.\n")
+        sys.exit()
+else:
+    command = sys.argv[1]
+    param = sys.argv[2]
+    if command == '--brightness':
+        arr = doBrightness(arr, param)
+    elif command == '--contrast':
+        arr = doContrast(arr, param)
+    else:
+        print("Unknown command: " + command)
+        sys.exit()
+
+newIm = Image.fromarray(arr.astype(np.uint8))
+newIm.save("result.bmp")
